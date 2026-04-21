a54227c62dacef3dd1252fd005f09de5d2475708
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2026-04-21 09:24:07 -0500

    Estructurando 2...

---
+++ LAB01/Resultados/Caso01/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+        CubbyHole cub = new CubbyHole();
+        Consumidor cons = new Consumidor(cub, 1);
+        Productor prod = new Productor(cub, 1);
+
+        prod.start();
+        cons.start();
+    }
+}
