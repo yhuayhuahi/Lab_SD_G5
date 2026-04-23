b19c6c07988f60e6a03e2b69646b2961a2696f88
Rommel0569 <rchambive@unsa.edu.pe>
2026-04-23 13:20:49 -0500

    Algortimo christian

---
+++ LAB02/propuestos/Alg_christian/CristianServer.java
@@ -0,0 +1,11 @@
+public class CristianServer {
+    private double serverTime;
+
+    public CristianServer(double startTime) {
+        this.serverTime = startTime;
+    }
+
+    public double getServerTime() {
+        return serverTime;
+    }
+}
