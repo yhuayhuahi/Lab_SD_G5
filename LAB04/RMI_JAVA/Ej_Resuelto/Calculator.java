2babceeeb5911f6885b3badcdfba7ba1f89edbca
jdaeL <joedaniel0804@gmail.com>
2026-05-11 22:13:19 -0500

    Agregando ejercicio resuelto

---
+++ LAB04/RMI_JAVA/Ej_Resuelto/Calculator.java
@@ -0,0 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Calculator extends Remote {
+    public int add(int a, int b) throws RemoteException;
+    public int sub(int a, int b) throws RemoteException;
+    public int mul(int a, int b) throws RemoteException;
+    public int div(int a, int b) throws RemoteException;
+}
