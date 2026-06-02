b609b9d2fb25cf7626c4ef2ba9e0dd59857994c6
CIELO CRISTAL MEZA VIZCARRA <cmezav@unsa.edu.pe>
2026-06-02 10:06:13 -0500

    Más operaciones agregadas

---
+++ LAB07/resuelto/CalculadoraSOAP/src/main/java/ClienteSOAP.java
@@ -0,0 +1,21 @@
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.URL;
+
+public class ClienteSOAP {
+
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("http://localhost:8080/calculadora?wsdl");
+
+        QName qname = new QName("http:///", "CalculadoraSOAPService");
+
+        Service service = Service.create(url, qname);
+
+        CalculadoraSOAP calculadora = service.getPort(CalculadoraSOAP.class);
+
+        System.out.println("Suma: " + calculadora.sumar(8, 4));
+        System.out.println("Resta: " + calculadora.restar(8, 4));
+        System.out.println("Multiplicación: " + calculadora.multiplicar(8, 4));
+        System.out.println("División: " + calculadora.dividir(8, 4));
+    }
+}
